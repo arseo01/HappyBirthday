df44a296685c47d06c526c75f3af5aeeea858587
Dell <arseoazemaj@gmail.com>
2024-10-12 20:50:42 +0200

    afs

--- Program.cs
+++ Program.cs
@@ -12,7 +12,7 @@ builder.Services.AddCors(options =>
     options.AddPolicy(name: "localhostDevelopment",
                       policy =>
                       {
-                          policy.WithOrigins("http://127.0.0.1:5500")
+                          policy.WithOrigins(origins: "https://arseo01.github.io/HappyBirthday/")
                             .AllowAnyHeader()
                             .AllowAnyMethod();
                       });
