947401e8fe45cf654e95ee0d83319001fd77d8d1
Dell <arseoazemaj@gmail.com>
2024-10-18 21:48:31 +0200

    enable CORS for github pages

--- Program.cs
+++ Program.cs
@@ -12,7 +12,7 @@ builder.Services.AddCors(options =>
     options.AddPolicy(name: "localhostDevelopment",
                       policy =>
                       {
-                          policy.WithOrigins("http://127.0.0.1:5500")
+                          policy.WithOrigins("http://127.0.0.1:5500", "https://celebratebirthdays.github.io", "http://127.0.0.1:5501")
                             .AllowAnyHeader()
                             .AllowAnyMethod();
                       });
