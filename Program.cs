712846a911964b88139b7c503fd82a832cef974f
Dell <arseoazemaj@gmail.com>
2024-10-25 20:28:11 +0200

    fix a CORS error

--- Program.cs
+++ Program.cs
@@ -12,7 +12,7 @@ builder.Services.AddCors(options =>
     options.AddPolicy(name: "localhostDevelopment",
                       policy =>
                       {
-                          policy.WithOrigins("http://127.0.0.1:5500", "https://celebratebirthdays.github.io", "http://127.0.0.1:5501")
+                          policy.WithOrigins("http://127.0.0.1:5500", "http://127.0.0.1:5501", "https://celebratebirthdays.github.io", "https://arseo01.github.io")
                             .AllowAnyHeader()
                             .AllowAnyMethod();
                       });
