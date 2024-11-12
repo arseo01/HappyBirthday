2c41b872e7c25dc56ffa57b2c1cb5b796a5780e5
Dell <arseoazemaj@gmail.com>
2024-11-12 17:18:54 +0100

    changeing

--- Program.cs
+++ Program.cs
@@ -1,9 +1,7 @@
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
@@ -13,21 +11,19 @@ builder.Services.AddCors(options =>
                       policy =>
                       {
                           policy.WithOrigins("http://127.0.0.1:5500", "https://arseo01.github.io", "http://127.0.0.1:5501", "https://celebratebirthday.github.io")
-                            .AllowAnyHeader()
-                            .AllowAnyMethod();
+                                .AllowAnyHeader()
+                                .AllowAnyMethod();
                       });
 });
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
+app.UseSwagger();
+app.UseSwaggerUI();
 
-app.UseHttpsRedirection();
+// Commented out HTTPS redirection for Render compatibility
+// app.UseHttpsRedirection();
 
 app.UseCors("localhostDevelopment");
 
